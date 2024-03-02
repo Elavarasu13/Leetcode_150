628bf994450ccb970e988e5b4ad85a1370007aad
Elavarasu13 <elavarasu.subramaniam@gmail.com>
2024-03-02 13:58:49 +0530

    feat: new source file

---
+++ Majority_element.java
@@ -0,0 +1,35 @@
+package Leetcode_150;
+
+public class Majority_element {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int nums[] = {3,2,3};
+		System.out.println(MajorityElement(nums));
+
+	}
+	public static int MajorityElement(int[] nums)
+	{
+		int cand  = 0;
+		int count  = 0;
+		for(int i=0;i<nums.length;i++)
+		{
+			if(count == 0)
+			{
+				cand = nums[i];
+			}
+			
+			if(cand == nums[i])
+			{
+				count++;
+			}
+			else
+			{
+				count--;
+			}
+		}
+		return cand;
+	}
+
+}
